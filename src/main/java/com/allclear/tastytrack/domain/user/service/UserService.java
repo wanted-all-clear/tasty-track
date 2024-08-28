7af74c3ca568174e74bdcdbd0f342a572a86f66c
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-29 01:51:07 +0900

    [Feat] 회원 정보 수정 기능 구현

--- src/main/java/com/allclear/tastytrack/domain/user/service/UserService.java
+++ src/main/java/com/allclear/tastytrack/domain/user/service/UserService.java
@@ -1,6 +1,8 @@
 package com.allclear.tastytrack.domain.user.service;
 
 import com.allclear.tastytrack.domain.user.dto.UserCreateRequest;
+import com.allclear.tastytrack.domain.user.dto.UserInfo;
+import com.allclear.tastytrack.domain.user.dto.UserUpdateRequest;
 import org.springframework.http.HttpHeaders;
 
 public interface UserService {
@@ -8,5 +10,6 @@ public interface UserService {
     void signup(UserCreateRequest userCreateRequest);
 
     HttpHeaders signin(UserCreateRequest userCreateRequest);
+    UserInfo updateUserInfo(String username, UserUpdateRequest userUpdateRequest);
 
 }
