d2dd51288f2fe0f6efa66928564275c1125c7489
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-29 01:15:04 +0900

    [Feat] 사용자 로그인 기능 구현

--- src/main/java/com/allclear/tastytrack/domain/user/controller/UserController.java
+++ src/main/java/com/allclear/tastytrack/domain/user/controller/UserController.java
@@ -4,6 +4,8 @@ import com.allclear.tastytrack.domain.user.dto.UserCreateRequest;
 import com.allclear.tastytrack.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,5 +28,15 @@ public class UserController {
         return ResponseEntity.status(201).body("회원가입이 완료되었습니다.");
 
     }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<String> signin(@Validated @RequestBody UserCreateRequest userCreateRequest) {
+
+        HttpHeaders httpHeaders = userService.signin(userCreateRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).headers(httpHeaders).body(null);
+    }
+
+
 
 }
