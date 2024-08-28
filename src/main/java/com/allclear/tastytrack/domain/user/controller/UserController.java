d7f51904c853f48516123381ac116fa86d7cfd12
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-29 00:53:31 +0900

    [Feat] 회원가입 기능 구현

---
+++ src/main/java/com/allclear/tastytrack/domain/user/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.allclear.tastytrack.domain.user.controller;
+
+import com.allclear.tastytrack.domain.user.dto.UserCreateRequest;
+import com.allclear.tastytrack.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+@Slf4j
+public class UserController {
+
+    private final UserService userService;
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<String> signup(@Validated @RequestBody UserCreateRequest userCreateRequest) {
+
+        userService.signup(userCreateRequest);
+        return ResponseEntity.status(201).body("회원가입이 완료되었습니다.");
+
+    }
+
+}
