3c1c69564cbbcbc6f6b04871341d8dff0ef2e1a0
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-28 09:42:22 +0900

    [Init] 프로젝트 생성

---
+++ src/main/java/com/allclear/tastytrack/TastytrackApplication.java
@@ -0,0 +1,13 @@
+package com.allclear.tastytrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastytrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastytrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allclear/tastytrack/TastytrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allclear.tastytrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastytrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
