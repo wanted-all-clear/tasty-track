c23c03d22b36ef514b4596dd8a0cc9d663ac499a
Eunjung <fkznsha23@gmail.com>
2024-08-30 01:45:48 +0900

    [Del] 불필요한 import 삭제

--- src/main/java/com/allclear/tastytrack/domain/restaurant/controller/RestaurantController.java
+++ src/main/java/com/allclear/tastytrack/domain/restaurant/controller/RestaurantController.java
@@ -6,7 +6,6 @@ import java.util.concurrent.CompletableFuture;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
