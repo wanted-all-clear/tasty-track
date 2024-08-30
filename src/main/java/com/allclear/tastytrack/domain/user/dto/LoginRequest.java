5b7c31b12bd4118f343b5a14d5845a2e2bb99244
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-30 16:21:36 +0900

    [Refactor] 로그인 시 요청 데이터 수정

---
+++ src/main/java/com/allclear/tastytrack/domain/user/dto/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.allclear.tastytrack.domain.user.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class LoginRequest {
+    @NotNull(message = "사용자 이름은 필수 입력 값입니다.")
+    private String username;
+    @NotNull(message = "비밀번호는 필수 입력 값입니다.")
+    private String password;
+}
