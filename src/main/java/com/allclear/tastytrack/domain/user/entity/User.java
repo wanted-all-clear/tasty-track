c4d8f5cc64ca2b58e16e79af501d2cc11d1e8753
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-08-30 22:59:49 +0900

    [Refactor] User id 타입 변경 Integer -> int

--- src/main/java/com/allclear/tastytrack/domain/user/entity/User.java
+++ src/main/java/com/allclear/tastytrack/domain/user/entity/User.java
@@ -16,7 +16,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private int id;
 
     @Column(nullable = false)
     private String username;
