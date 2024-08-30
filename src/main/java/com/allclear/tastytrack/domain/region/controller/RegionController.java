b4d0e44b645ec18fb826556ca42c95546fdc4d40
bsjin1122 <kitty7579@hanmail.net>
2024-08-30 12:47:30 +0900

    [Feat] 시군구 정보 조회 기능 완료(테스트 포함)

---
+++ src/main/java/com/allclear/tastytrack/domain/region/controller/RegionController.java
@@ -0,0 +1,45 @@
+package com.allclear.tastytrack.domain.region.controller;
+
+import com.allclear.tastytrack.domain.region.dto.RegionGroupResponse;
+import com.allclear.tastytrack.domain.region.dto.RegionResponse;
+import com.allclear.tastytrack.domain.region.service.RegionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/regions")
+@Tag(name = "Region", description = "지역 관련 API")
+public class RegionController {
+
+    private final RegionService regionService;
+
+    @Operation(summary = "지역 정보 조회", description = "서울특별시 지역구별 검색을 위한 지역 정보 조회 API입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "리스트를 성공적으로 조회하였습니다.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RegionResponse.class))),
+            @ApiResponse(responseCode = "404", description = "지역 정보를 찾을 수 없습니다.",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류",
+                    content = @Content)
+    })
+    @GetMapping("")
+    public ResponseEntity<RegionGroupResponse> getRegions(){
+
+        RegionGroupResponse responses = regionService.getRegionInfo();
+
+        return ResponseEntity.ok(responses);
+    }
+}
--- src/main/java/com/allclear/tastytrack/global/exception/ErrorCode.java
+++ src/main/java/com/allclear/tastytrack/global/exception/ErrorCode.java
@@ -13,7 +13,11 @@ public enum ErrorCode {
 	USER_NOT_EXIST(HttpStatus.NOT_FOUND, "가입되지 않은 아이디입니다."),
 
 	NOT_VALID_PROPERTY(HttpStatus.BAD_REQUEST, "입력 값을 확인해주세요"),
-	NOT_EXISTENT_RESTAURANT(HttpStatus.BAD_REQUEST, "조회할 수 없는 음식점입니다.");
+	NOT_EXISTENT_RESTAURANT(HttpStatus.BAD_REQUEST, "조회할 수 없는 음식점입니다."),
+
+	// Region
+	NO_REGION_DATA(HttpStatus.NOT_FOUND, "지역 정보 데이터가 없습니다."),
+	DATABASE_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "데이터베이스 처리 중 오류가 발생했습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
