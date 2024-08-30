876b2ff3f039a19480be1628bfca9574f181c8c5
Eunjung <fkznsha23@gmail.com>
2024-08-30 17:06:40 +0900

    [Test] 맛집 평점 업데이트 테스트 작성 [Feat] 맛집 평점 업데이트 기능 구현 [Perf] 맛집 상세정보 조회 기능 수월한 테스트를 위해 개선

--- src/test/java/com/allclear/tastytrack/restaurant/RestaurantServiceImplTest.java
+++ src/test/java/com/allclear/tastytrack/restaurant/RestaurantServiceImplTest.java
@@ -2,8 +2,10 @@ package com.allclear.tastytrack.restaurant;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.allclear.tastytrack.domain.restaurant.entity.Restaurant;
 import com.allclear.tastytrack.domain.restaurant.repository.RestaurantRepository;
 import com.allclear.tastytrack.domain.restaurant.service.RestaurantServiceImpl;
+import com.allclear.tastytrack.global.exception.ErrorCode;
 
 @ExtendWith(MockitoExtension.class)
 public class RestaurantServiceImplTest {
@@ -23,8 +26,13 @@ public class RestaurantServiceImplTest {
     @InjectMocks
     private RestaurantServiceImpl restaurantServiceImpl;
 
-    @DisplayName("맛집 조회의 해피 테스트 입니다.")
+    @BeforeEach
+    public void setUp() {
+
+    }
+
     @Test
+    @DisplayName("맛집 조회의 해피 테스트 입니다.")
     public void getRestaurantDetailSuccessTest() {
         //given
         Restaurant restaurant = Restaurant.builder()
@@ -35,26 +43,44 @@ public class RestaurantServiceImplTest {
                 .rateScore(4.2)
                 .oldAddress("old address")
                 .newAddress("new address")
-                .lon("위도")
-                .lat("경도")
+                .lon(3123.1231)
+                .lat(134323.13212)
                 .build();
         given(restaurantRepository.findRestaurantByIdAndDeletedYn(anyInt(), anyBoolean())).willReturn(restaurant);
 
         // when
-        restaurantServiceImpl.getRestaurant(anyInt());
+        restaurantServiceImpl.getRestaurant(anyInt(), anyBoolean());
 
         //then
-        verify(restaurantRepository, times(1)).findById(anyInt());
+        verify(restaurantRepository, times(1)).findRestaurantByIdAndDeletedYn(anyInt(), anyBoolean());
     }
 
-    @DisplayName("맛집 조회의 실패 테스트 입니다.")
     @Test
+    @DisplayName("맛집 조회의 실패 테스트 입니다.")
     public void getRestaurantDetailFailTest() {
         // when
-        Throwable ex = assertThrows(RuntimeException.class, () -> restaurantServiceImpl.getRestaurant(anyInt()));
+        Throwable ex = assertThrows(RuntimeException.class,
+                () -> restaurantServiceImpl.getRestaurant(anyInt(), anyBoolean()));
 
         // then
-        assertThat(ex.getMessage()).isEqualTo("조회된 음식점이 존재하지 않습니다.");
+        assertThat(ex.getMessage()).isEqualTo(ErrorCode.NOT_VALID_PROPERTY.getMessage());
     }
+
+    @Test
+    @DisplayName("맛집 평점 업데이트 기능 성공 테스트입니다.")
+    public void updateRestaurantScore() {
+        // given
+        Double beforeScore = 4.0;
+        int beforeReviewCount = 200;
+        int score = 3;
+
+        // when
+        Double result = restaurantServiceImpl.updateRestaurantScore(beforeScore, beforeReviewCount, score);
+
+        // then
+        assertThat(result).isEqualTo(((beforeScore * beforeReviewCount) + score) / (beforeReviewCount + 1));
+
+    }
+
 
 }
