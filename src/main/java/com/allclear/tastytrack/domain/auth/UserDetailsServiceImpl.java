b71c397d48a95bc90fbf8461e565a2cd920942c1
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-09-01 13:17:58 +0900

    [Refactor] 토큰 검증 예외 처리 수정

--- src/main/java/com/allclear/tastytrack/domain/auth/UserDetailsServiceImpl.java
+++ src/main/java/com/allclear/tastytrack/domain/auth/UserDetailsServiceImpl.java
@@ -2,8 +2,6 @@ package com.allclear.tastytrack.domain.auth;
 
 import com.allclear.tastytrack.domain.user.entity.User;
 import com.allclear.tastytrack.domain.user.repository.UserRepository;
-import com.allclear.tastytrack.global.exception.CustomException;
-import com.allclear.tastytrack.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,7 +16,7 @@ public class UserDetailsServiceImpl {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_EXIST));
+                .orElseThrow(() -> new UsernameNotFoundException("가입되지 않은 아이디입니다."));
 
         return new UserDetailsImpl(user);
     }
