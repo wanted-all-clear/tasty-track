28098bc5cca75e2a8f734c1a4b9f9d31105f2556
Eunjung <fkznsha23@gmail.com>
2024-09-02 18:53:19 +0900

    [Perf] Restaurant, Review, User 연관관계 매핑

--- src/main/java/com/allclear/tastytrack/domain/restaurant/dto/RestaurantDetail.java
+++ src/main/java/com/allclear/tastytrack/domain/restaurant/dto/RestaurantDetail.java
@@ -4,19 +4,25 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.allclear.tastytrack.domain.review.dto.ReviewResponse;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
 @Builder
+@NoArgsConstructor
 public class RestaurantDetail {
 
     private String name;
-
 
     private String type;
 
@@ -32,6 +38,9 @@ public class RestaurantDetail {
 
     private Double lat;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime lastUpdateAt;
 
     @Setter
--- src/main/java/com/allclear/tastytrack/domain/review/dto/ReviewResponse.java
+++ src/main/java/com/allclear/tastytrack/domain/review/dto/ReviewResponse.java
@@ -2,32 +2,30 @@ package com.allclear.tastytrack.domain.review.dto;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.CreatedDate;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReviewResponse implements Comparable<ReviewResponse> {
 
     private String username;
     private int score;
     private String content;
-    @CreatedDate
+
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
     @Override
