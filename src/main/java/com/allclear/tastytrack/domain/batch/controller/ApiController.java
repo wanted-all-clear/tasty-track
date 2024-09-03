40ae7871d2ddd2bcd1cd31b58dd2b79f2f605e0f
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-09-03 23:51:22 +0900

    [Refactor] BatchConfig 리팩토링 및 ApiController 엔드포인트 수정

--- src/main/java/com/allclear/tastytrack/domain/batch/controller/ApiController.java
+++ src/main/java/com/allclear/tastytrack/domain/batch/controller/ApiController.java
@@ -5,28 +5,31 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/api/fetch")
 public class ApiController {
 
     private final JobLauncher jobLauncher;
     private final JobRegistry jobRegistry;
 
     // TODO: 테스트용으로 Controller API 호출중이며, 추후 스케줄링으로 전환할 예정입니다.
-    @GetMapping("/first")
-    public String firstApi(@RequestParam("value") String value) throws Exception {
+    @GetMapping("/{value}")
+    public ResponseEntity<String> firstApi(@PathVariable String value) throws Exception {
 
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString("date", value)
                 .toJobParameters();
 
-        jobLauncher.run(jobRegistry.getJob("initRestaurantDataJob"), jobParameters);
+        jobLauncher.run(jobRegistry.getJob("fetchJob"), jobParameters);
 
-        return "ok";
+        return ResponseEntity.ok("배치 작업이 완료되었습니다.");
     }
 
 }
