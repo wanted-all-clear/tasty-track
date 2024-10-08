2939018dc3f42f3cde6c689abe83abadc5bfa3ce
ohyeryung <dpfu1948@g.eulji.ac.kr>
2024-10-09 03:54:11 +0900

    [Refactor] 배포 시 필요한 Redis 환경 설정 추가

---
+++ src/main/java/com/allclear/tastytrack/global/config/RedisConfig.java
@@ -0,0 +1,45 @@
+package com.allclear.tastytrack.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${REDIS_HOST}")
+    private String redisHost;
+
+    @Value("${REDIS_PORT}")
+    private int redisPort;
+
+    @Value(("${REDIS_PASSWORD}"))
+    private String password;
+
+    @Bean
+    public RedisConnectionFactory connectionFactory() {
+
+        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
+        redisConfiguration.setHostName(redisHost);
+        redisConfiguration.setPort(redisPort);
+        redisConfiguration.setPassword(password);
+        return new LettuceConnectionFactory(redisConfiguration);
+    }
+
+    @Bean
+    RedisTemplate<String, Object> redisTemplate() {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setConnectionFactory(connectionFactory());
+        return template;
+    }
+
+}
